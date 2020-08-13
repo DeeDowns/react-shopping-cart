87e13409704fdb47624af3073afdadcb89cb07ba
Dee Downs <deedowns@dees-mbp.home>
2020-08-13 15:53:17 -0400

    completed steps 5-7; created Cartcontext and provided data to ShoppingCart and Navigation; removed props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 import { ProductContext } from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -22,18 +23,18 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Route exact path="/">
-					<Products />
-				</Route>
+				<CartContext.Provider value={cart}>
+					<Navigation />
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+					<Route path="/cart">
+						<ShoppingCart  />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
 		</div>
 	);
 }
